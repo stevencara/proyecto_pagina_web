a50ba6ab28b9f0dda82a11cfcbeb37bc5aafe5fe
stevencara2009 <stevencara2009@gmail.com>
2024-05-27 15:55:40 -0500

    Inicio pagina index menu

--- src/index.js
+++ src/index.js
@@ -29,7 +29,7 @@ app.use(express.json());
 
 /* --------------------------------- Routes --------------------------------- */
 app.get('/', (req, res) => {
-    res.json({"message": "Hola mundo"})
+    res.render('index')
 });
 
 /* ------------------------------ Public files ------------------------------ */
