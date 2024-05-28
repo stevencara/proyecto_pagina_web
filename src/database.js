ab0ddd6117b0c978cd65c13077caaeb5f9c79f2e
stevencara2009 <stevencara2009@gmail.com>
2024-05-27 19:29:27 -0500

    se creo la vista y el metodo para listar

--- src/database.js
+++ src/database.js
@@ -3,7 +3,7 @@ import { createPool } from "mysql2/promise";
 const pool = createPool({
     host: 'localhost',
     port: '3306',
-    user: prueba02,
+    user: 'prueba02',
     password: 'prueba02',
     database: 'prueba02'
 });
