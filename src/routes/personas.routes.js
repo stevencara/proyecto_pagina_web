644c90c29208149db9ca1639309eefd916628553
stevencara2009 <stevencara2009@gmail.com>
2024-05-28 17:09:40 -0500

    se creo modulo de editar

--- src/routes/personas.routes.js
+++ src/routes/personas.routes.js
@@ -19,7 +19,7 @@ router.post('/add', async (req, res) => {
     } catch (error) {
         res.status(500).json({message: error.message});
     }
-})
+});
 
 router.get('/list', async(req, res) => {
     try {
@@ -39,5 +39,31 @@ router.get('/delete/:id', async(req, res) => {
         res.status(500).json({message: error.message});
     }
 });
+
+router.get('/edit/:id', async(req, res) => {
+    try {
+        const {id} = req.params
+        const [persona] = await pool.query('SELECT * FROM personas WHERE id = ?', [id]);
+        const personaEdit = persona[0];
+        res.render('personas/edit', {persona: personaEdit})
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
+});
+
+router.post('/edit/:id', async(req, res) => {
+    try {
+        const {id} = req.params
+        const {name, lastname, age} = req.body
+        const editPersona = {name, lastname, age} 
+        
+        await pool.query('UPDATE personas SET ? WHERE id = ?', [editPersona, id]);
+        res.redirect('/list');
+
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
+});
+
 
 export default router;  
